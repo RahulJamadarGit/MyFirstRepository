0598cdc2c3e13d39cb711ccbd08381d9cf030674
RahulJamadarGit <68692726+RahulJamadarGit@users.noreply.github.com>
2020-09-01 15:25:13 +0530

    Arithmetic Operation Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,20 @@ class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World....Welcome to GitHub!");
     System.out.println("Addition is: "+add(10,20));
-    System.out.println("Subtraction is: "+add(40,10));
+    System.out.println("Subtraction is: "+subtract(40,10));
+    System.out.println("Multiplicaton is: "+multiply(40,10));
+    System.out.println("Division is: "+divide(50,10));
   }
   public static int add(int a, int b){
    return a+b; 
   }
   public static int subtract(int a, int b){
    return a-b; 
+  }
+  public static int multiply(int a, int b){
+   return a*b; 
+  }
+  public static int divide(int a, int b){
+   return a/b; 
   }
 }
