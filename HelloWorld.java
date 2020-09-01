8f396cccfdb9c8221680c246572826472dfa2a47
RahulJamadarGit <68692726+RahulJamadarGit@users.noreply.github.com>
2020-09-01 15:17:22 +0530

    Addition logic added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ class HelloWorld {
 
   public static void main(String[] args){
     System.out.println("Hello World....Welcome to GitHub!");
+    System.out.println("Addition is: "+add(10,20));
+  }
+  public static int add(int a, int b){
+   return a+b; 
   }
 }
