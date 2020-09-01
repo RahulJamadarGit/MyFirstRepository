c4de73c30fe0f05a7c5c95d402c80f2daee66a8f
RahulJamadarGit <68692726+RahulJamadarGit@users.noreply.github.com>
2020-09-01 15:19:58 +0530

    Subtraction logic added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,12 @@ class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World....Welcome to GitHub!");
     System.out.println("Addition is: "+add(10,20));
+    System.out.println("Subtraction is: "+add(40,10));
   }
   public static int add(int a, int b){
    return a+b; 
+  }
+  public static int subtract(int a, int b){
+   return a-b; 
   }
 }
